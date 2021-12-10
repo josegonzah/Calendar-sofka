bca2a6434d33a043896f1f5c6d6465d0cf7f9c2c
josegonzah <jose.gonzalezh@udea.edu.co>
2021-12-10 14:15:33 -0500

    Primer todo

--- src/main/java/co/com/sofka/calendar/services/SchedulerService.java
+++ src/main/java/co/com/sofka/calendar/services/SchedulerService.java
@@ -15,6 +15,8 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 
 @Service
@@ -23,20 +25,18 @@ public class SchedulerService {
     @Autowired
     private ProgramRepository programRepository;
 
-    //TODO: deben retornar un flux de programDate Flux<ProgramDate>
-    public List<ProgramDate> generateCalendar(String programId, LocalDate startDate) {
+    public Flux<ProgramDate> generateCalendar(String programId, LocalDate startDate) {
         var endDate = new AtomicReference<>(LocalDate.from(startDate));
         final AtomicInteger[] pivot = {new AtomicInteger()};
         final int[] index = {0};
 
-        //TODO: debe pasarlo a reactivo, no puede trabaja elementos bloqueantes
-        //TODO: trabajar el map reactivo y no deben colectar
-        var program = programRepository.findById(programId).block();
-        return Optional.ofNullable(program)
-                .map(this::getDurationOf)
-                .orElseThrow(() -> new RuntimeException("El programa academnico no existe"))
+        var program = programRepository.findById(programId);
+
+        var result = program
+                .flatMapMany(programa -> Flux.fromStream(getDurationOf(programa)))
                 .map(toProgramDate(startDate, endDate, pivot[0], index))
-                .collect(Collectors.toList());
+                .switchIfEmpty(Mono.error(new RuntimeException("Objeto Vacio")));
+        return result;
     }
 
     //No tocar
--- src/test/java/co/com/sofka/calendar/CalendarApplicationTests.java
+++ src/test/java/co/com/sofka/calendar/CalendarApplicationTests.java
@@ -8,6 +8,7 @@ class CalendarApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
